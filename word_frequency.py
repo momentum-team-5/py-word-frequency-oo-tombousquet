924d8106d07c90da75ea8fbc61d5a797dcfd8e3f
Tom Bousquet <tom.bousquet@alumni.unc.edu>
2020-10-14 16:32:07 -0400

    top ten?

--- word_frequency.py
+++ word_frequency.py
@@ -1,25 +1,40 @@
+#!/usr/local/bin/python3
+
+from string import punctuation
+
 STOP_WORDS = [
     'a', 'an', 'and', 'are', 'as', 'at', 'be', 'by', 'for', 'from', 'has',
     'he', 'i', 'in', 'is', 'it', 'its', 'of', 'on', 'that', 'the', 'to',
     'were', 'will', 'with'
 ]
 
+# ALL RAISES SHOULD BE Deleted, can be treated like pass
+
+def ispunct(s):
+    if s == "":
+        return False
+    for c in s:
+        if c not in punctuation:
+            return False
+
+    return True        
 
 class FileReader:
     def __init__(self, filename):
-        pass
+        self.filename = filename
 
     def read_contents(self):
         """
         This should read all the contents of the file
         and return them as one string.
         """
-        raise NotImplementedError("FileReader.read_contents")
-
+        with open(self.filename) as wordsfile:
+            return wordsfile.read()
 
 class WordList:
     def __init__(self, text):
-        pass
+        self.text = text
+
 
     def extract_words(self):
         """
@@ -27,14 +42,36 @@ class WordList:
         is responsible for lowercasing all words and stripping
         them of punctuation.
         """
-        raise NotImplementedError("WordList.extract_words")
+        filtered_text = self.text.lower()        
+
+        for p in punctuation:
+            filtered_text = filtered_text.replace(p, '')
+
+        self.extracted_words = filtered_text.split()    
+
+        # for (i, word in enumerate(self.extract_words)):
+        #     filtered_letters = []
+
+        #     for letter in word:
+        #         if letter not in punctuation:
+        #             filtered_letters.append(letter)
+
+        #     filtered_word = "".join(filtered_letters)
+        #     self.extracted_words[i] = "".join(filtered_letters)
+
+
 
     def remove_stop_words(self):
         """
         Removes all stop words from our word list. Expected to
         be run after extract_words.
         """
-        raise NotImplementedError("WordList.remove_stop_words")
+        extracted_words = self.extracted_words
+
+        for s_w in STOP_WORDS:
+            while s_w in extracted_words:
+                self.stops_removed = extracted_words.remove(s_w)
+                
 
     def get_freqs(self):
         """
@@ -43,12 +80,20 @@ class WordList:
         extract_words and remove_stop_words. The data structure
         could be a dictionary or another type of object.
         """
-        raise NotImplementedError("WordList.get_freqs")
+
+        word_counts =  {}
+        for w in self.extracted_words:
+            if w in word_counts:
+                word_counts[w] += 1
+            else:
+                word_counts[w] = 1  
+        return word_counts          
 
 
 class FreqPrinter:
     def __init__(self, freqs):
-        pass
+        self.freqs = freqs
+
 
     def print_freqs(self):
         """
@@ -67,7 +112,10 @@ class FreqPrinter:
        rights | 6    ******
         right | 6    ******
         """
-        raise NotImplementedError("FreqPrinter.print_freqs")
+
+        
+
+        print(self.freqs)
 
 
 if __name__ == "__main__":
